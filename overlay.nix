215282cd9246c073996dc2a1c7411ea072c41d71
Ben Siraphob <bensiraphob@gmail.com>
2025-07-15 13:59:46 -0700

    flint: remove (in upstream)

--- overlay.nix
+++ overlay.nix
@@ -22,7 +22,6 @@ in
   double-pendulum-simulation = prev.haskellPackages.callPackage ./pkgs/double-pendulum-simulation { };
   essentials-of-compilation = callPackage ./pkgs/essentials-of-compilation { };
   fe = callPackage ./pkgs/fe { };
-  flint = callPackage ./pkgs/flint { };
   git2graph = callPackage ./pkgs/git2graph { };
   jonesforth = callPackage ./pkgs/jonesforth { };
   lang-narrow = prev.ocamlPackages.callPackage ./pkgs/lang-narrow { };
