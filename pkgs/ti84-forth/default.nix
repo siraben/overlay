ccb4a5b373d528de044e3bcd9883279c9c913358
Ben Siraphob <bensiraphob@gmail.com>
2025-07-15 00:10:08 -0700

    ti84-forth: fix

--- pkgs/ti84-forth/default.nix
+++ pkgs/ti84-forth/default.nix
@@ -5,6 +5,6 @@
     owner = "siraben";
     repo = "ti84-forth";
     rev = "97162514f81dce267cafdbd021978efa181177d6";
-    sha256 = "1m9ig05c8vzsshn3ihjfwg4a13rg7b3fvg0kz7kbmgvjwiri1h4y";
+    hash = "sha256-nsAQc+Ryv7rm+RO87cY6L4+gyONOwjgs1PpvxAp4MdU=";
   })
   { inherit pkgs; })
