d3add5f58a87690e9b2410b9efc85b23615835dc
Ben Siraphob <bensiraphob@gmail.com>
2025-07-15 10:08:52 -0700

    micro-lisp: update to fork

--- pkgs/micro-lisp/default.nix
+++ pkgs/micro-lisp/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "micro-lisp";
 
   src = fetchFromGitHub {
-    owner = "carld";
+    owner = "alve1801";
     repo = "micro-lisp";
     rev = "faa448de9c7e407ad1d31bb7ec0c10f2367fc736";
     sha256 = "0ignsvn7vhrgyy60nzbjllngrkkc73x19gsyd3fnpa9vkapdsr40";
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "A very small Lisp interpreter in under 200 lines of C";
-    homepage = "https://github.com/carld/micro-lisp";
+    homepage = "https://web.archive.org/web/20230526073728/https://carld.github.io/2017/06/20/lisp-in-less-than-200-lines-of-c.html";
     license = lib.licenses.mit;
     platforms = lib.platforms.all;
   };
