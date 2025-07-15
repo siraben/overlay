661f7c6abdd757f88d8ddf3a3474ba8e77669036
Ben Siraphob <bensiraphob@gmail.com>
2025-07-15 15:25:03 -0700

    runpodctl: fix

--- pkgs/runpodctl/default.nix
+++ pkgs/runpodctl/default.nix
@@ -2,22 +2,22 @@
 
 buildGoModule rec {
   pname = "runpodctl";
-  version = "1.7.0";
+  version = "1.14.5";
 
   src = fetchFromGitHub {
     owner = "runpod";
     repo = "runpodctl";
     rev = "v${version}";
-    sha256 = "sha256-FY6ZQbc5JOaqkGWyMDER2+V/kFxiJYZc6dfIWvkgraE=";
+    sha256 = "sha256-wRHf2Bh0jz9UqrjJmc2ZV3WSLDYl+9frZ8nD0qDay7g=";
   };
 
-  vendorHash = "sha256-RCGUVnJl2XbSJ/L/PGLC7g9x5Pnvdaz3NlVE2XHdQYE=";
+  vendorHash = "sha256-OGUt+L0wP6eQkY/HWL+Ij9z9u+wsQ5OLK/IAq+1ezVA=";
 
   meta = with lib; {
     description = "RunPod CLI for pod management";
     homepage = "https://github.com/runpod/runpodctl";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ siraben ];
-    mainProgram = "cli";
+    mainProgram = "runpodctl";
   };
 }
