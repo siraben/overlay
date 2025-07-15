24df05e62f12e3f02ecf0201a1a9a0344f78b217
Ben Siraphob <bensiraphob@gmail.com>
2025-07-15 12:42:20 -0700

    fe: fix

--- pkgs/fe/default.nix
+++ pkgs/fe/default.nix
@@ -1,19 +1,22 @@
-{ lib, stdenv, fetchFromGitHub, rustPlatform, libiconv }:
+{ lib, stdenv, fetchFromGitHub, rustPlatform, libiconv, pkg-config, openssl }:
 
 rustPlatform.buildRustPackage rec {
   pname = "fe";
-  version = "v0.15.0-alpha";
+  version = "v0.26.0";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "fe";
     rev = version;
-    sha256 = "sha256-HKzrxodFdX0je33xnJIwK9uvkHT/M8q/rl2S/vFODMo=";
+    sha256 = "sha256-zWwxcW3Osx5tKbl+6y8Tup4SfS+G7svqtooW82WycXE=";
   };
 
-  buildInputs = [ libiconv ];
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ libiconv openssl ];
 
-  cargoSha256 = "sha256-FfYUKDqCx6AP4xOCFwxWZarq0L9B3sZDUxWCPw1UIz0=";
+  cargoHash = "sha256-9cNCIlTGbkDtx7yzXtOszsjXCSLki2JQFIGG24p38EA=";
+
+  doCheck = false;
 
   meta = with lib; {
     description = "A statically typed smart contract language for the Ethereum Virtual Machine";
