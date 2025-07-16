5d8e8895366b43df50f17dfad7683de2da6e6448
Ben Siraphob <bensiraphob@gmail.com>
2025-07-15 23:33:27 -0700

    c64-non-sense: fix linux

--- pkgs/c64-non-sense/default.nix
+++ pkgs/c64-non-sense/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, cc65 }:
+{ lib, stdenv, fetchFromGitHub, cc65, vice }:
 
 stdenv.mkDerivation rec {
   pname = "c64-non-sense";
@@ -11,16 +11,24 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-4Ghb7gWTnapGAugXB0jfhLsf6d4Yuw57V5CmG2ueDno=";
   };
 
-  nativeBuildInputs = [ cc65 ];
+  nativeBuildInputs = [ cc65 vice ];
   buildInputs = [  ];
 
-  makeFlags = [ "CC=${cc65}/bin/cc65" ];
+  makeFlags = [ "CC=${cc65}/bin/cc65" "AS=${cc65}/bin/ca65" "LD=${cc65}/bin/cl65" "D64PACK=${vice}/bin/c1541" ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/c64-non-sense
+    cp src/non-sense.prg $out/share/c64-non-sense/
+    cp src/non-sense.d64 $out/share/c64-non-sense/
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "C64 NonSense Game Engine";
     homepage = "https://github.com/YouDirk/c64-non-sense";
     license = licenses.agpl3Plus;
     maintainers = with maintainers; [ siraben ];
-    platforms = platforms.all;
+    platforms = platforms.linux;
   };
 }
