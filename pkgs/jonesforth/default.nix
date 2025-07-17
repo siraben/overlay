3c1e77b11fbb1a1b13717ca47115f6ce6e8c06f3
Ben Siraphob <bensiraphob@gmail.com>
2025-07-16 22:42:53 -0700

    jonesforth: can be built on 64-bit

--- pkgs/jonesforth/default.nix
+++ pkgs/jonesforth/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub }:
+{ lib, stdenv, fetchFromGitHub, gcc }:
 
 stdenv.mkDerivation rec {
   name = "jonesforth";
@@ -8,6 +8,9 @@ stdenv.mkDerivation rec {
     rev = "4f853252f715132e7716cbd44e5306cefb6a6fec";
     sha256 = "1ns2s1dpxpqi3gqfz2km7dzpnp2xh5din7p3dvbp9p244camr534";
   };
+  
+  nativeBuildInputs = [ gcc ];
+  
   installPhase = ''
     mkdir -p $out/bin $out/share
     cp jonesforth $out/bin
@@ -19,6 +22,6 @@ stdenv.mkDerivation rec {
     description = "Mirror of Richard WM Jones's excellent literate x86 assembly implementation of Forth";
     homepage = "https://github.com/nornagon/jonesforth";
     maintainers = with maintainers; [ siraben ];
-    platforms = platforms.unix;
+    platforms = platforms.i686 ++ platforms.x86_64;
   };
 }
