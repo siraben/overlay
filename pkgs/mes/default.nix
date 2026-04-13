30868f559134bd40052bcdfd56f3bb9f53a3ffec
Ben Siraphob <bensiraphob@gmail.com>
2026-04-13 16:23:19 -0700

    mes: use env.CFLAGS for -std=gnu89, deduplicate warning flags

--- pkgs/mes/default.nix
+++ pkgs/mes/default.nix
@@ -25,19 +25,14 @@ stdenv.mkDerivation rec {
 
   buildInputs = lib.optional stdenv.isDarwin darwin.cctools;
 
+  env.CFLAGS = "-std=gnu89";
   env.NIX_CFLAGS_COMPILE = toString [
-    "-std=gnu89"
-    "-Wno-error=implicit-function-declaration"
     "-Wno-implicit-function-declaration"
-    "-Wno-error=incompatible-pointer-types"
     "-Wno-incompatible-pointer-types"
-    "-Wno-error=int-conversion"
     "-Wno-int-conversion"
-    "-Wno-error=implicit-int"
     "-Wno-implicit-int"
   ];
 
-  # Patch GCC 14+ / C99 incompatibilities
   postPatch = ''
     # Fix C99 for-loop initial declaration not allowed in gnu89 mode
     sed -i 's|for (long i = 0;|{ long i; for (i = 0;|' src/mescc-posix.c
